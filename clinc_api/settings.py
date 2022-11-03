fc87b1e52af162da645bac186543c1d4be451e97
moatazmohamed <mohamed.moataz194@gmail.com>
2022-11-03 21:53:51 +0200

    update settings for the server

--- clinc_api/settings.py
+++ clinc_api/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3!8mnjw^k($=xf%7p-ecq3-)798nauyvgv*f@&l3hb7q$d!6(6'
+SECRET_KEY = 'django-insecure-3!8mnjw^k($=xf%7p-ecq3-)798nauyvgv*f@&l@#$@#$#2#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'doctor',
     'nurse',
     'users',
-    'django_extensions',
+#    'django_extensions',
     'rest_framework'
 ]
 
@@ -126,7 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
